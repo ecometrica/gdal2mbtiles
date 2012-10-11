5e837e1f42626d717b933ee70cc4d06d6c1f5292
Simon Law <simon.law@ecometrica.com>
2012-10-10 20:23:36 -0400

    Use select.select() in ApplyResult and Pool to prevent spinning on Queues.

--- gdal2mbtiles/pool.py
+++ gdal2mbtiles/pool.py
@@ -2,6 +2,7 @@ from collections import deque
 from functools import wraps
 from multiprocessing import cpu_count, Process, Queue, TimeoutError
 from Queue import Empty as QueueEmpty
+from select import select
 
 
 class ApplyResult(object):
@@ -20,6 +21,34 @@ class ApplyResult(object):
         self._success = None
         self._result = None
 
+    @classmethod
+    def select(cls, rlist=[], wlist=[], timeout=None):
+        """
+        Like select(), returns (rready, wready) tuple of ApplyResults.
+
+        rlist is an iterable of ApplyResult objects that should be ready for
+        reading. wlist is the same, but for writing.
+
+        If timeout is None, blocks until one of the ApplyResult objects is
+        ready. Otherwise, it is time in seconds.
+
+        rready is a list of ApplyResult objects that are ready for
+        reading. wready is the same for writing.
+        """
+        rready, wready, xready = select(
+            [e._queue._reader for e in rlist],
+            [e._queue._writer for e in wlist],
+            [],
+            timeout
+        )
+        if rready:
+            rdict = dict((e._queue._reader, e) for e in rlist)
+            rready = [rdict[f] for f in rready]
+        if wready:
+            wdict = dict((e._queue._writer, e) for e in rlist)
+            wready = [wdict[f] for f in wready]
+        return (rready, wready)
+
     def ready(self):
         """Returns True if worker has finished."""
         return self._success is not None
@@ -29,8 +58,7 @@ class ApplyResult(object):
         assert self.ready()
         return self._success
 
-    def wait(self, timeout=None):
-        """Waits for worker to finish."""
+    def _wait(self, timeout, maintain_pool):
         assert self._process is not None
         if self.ready():
             return
@@ -47,11 +75,16 @@ class ApplyResult(object):
         if self.callback and self._success:
             self.callback(self._result)
 
-        self._pool._maintain(timeout=0)
+        if maintain_pool:
+            self._pool._maintain(timeout=0)
+
+    def wait(self, timeout=None):
+        """Waits for worker to finish."""
+        self._wait(timeout=timeout, maintain_pool=True)
 
     def get(self, block=True, timeout=None):
         """Returns the result from the worker or raises an exception."""
-        self.wait(timeout=timeout)
+        self._wait(timeout=timeout, maintain_pool=True)
         if not self.ready():
             raise TimeoutError
         if not self._success:
@@ -77,7 +110,6 @@ class ApplyResult(object):
                 queue.put((True, target(*args, **kwargs)))
             except Exception as e:
                 queue.put((False, e))
-        wrapped._target = True
         return wrapped
 
 
@@ -112,15 +144,15 @@ class Pool(object):
     def join(self):
         """Waits for all workers to finish."""
         while self._pending or self._pool:
-            self._maintain(timeout=1)
+            self._maintain(timeout=None)
 
     def _maintain(self, timeout):
         """Cleanup any exited workers and start replacements for them."""
         # Collect dead processes
-        for apply_result in list(self._pool):
-            apply_result.wait(timeout=timeout)
-            if apply_result.ready() and apply_result in self._pool:
-                self._pool.remove(apply_result)
+        ready = ApplyResult.select(rlist=self._pool, timeout=timeout)[0]
+        for apply_result in ready:
+            apply_result._wait(timeout=0, maintain_pool=False)
+            self._pool.remove(apply_result)
 
         # Create more processes
         while self._pending and len(self._pool) < self._processes:
