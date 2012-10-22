9e2d4af74d4a392e890610e546214adc5048d437
Simon Law <simon.law@ecometrica.com>
2012-10-22 15:30:20 -0400

    JpegRenderer and PngRenderer now take compression and other VIPS options.
    Also, fix JpegRenderer and PngRenderer.suffix handling.

--- gdal2mbtiles/renderers.py
+++ gdal2mbtiles/renderers.py
@@ -26,35 +26,81 @@ class JpegRenderer(Renderer):
 
     Since JPEGs cannot contain transparent areas, the alpha channel is
     discarded.
+
+    compression: JPEG compression level. Default 75.
+    interlace: Filename of ICC profile. Default None.
+    suffix: Suffix for filename. Default '.jpeg'.
     """
     _suffix = '.jpeg'
 
-    def render(self, image):
-        """
-        Renders the VIPS `image` to `filename`.
+    def __init__(self, compression=None, profile=None, **kwargs):
+        if compression is None:
+            compression = 75
+        _compression = int(compression)
+        if not 0 <= _compression <= 100:
+            raise ValueError(
+                'compression must be between 0 and 100: {0}'.format(compression)
+            )
+        self.compression = _compression
 
-        Returns the filename actually rendered to.
-        """
+        if profile is None:
+            profile = 'none'
+        self.profile = profile
+
+        super(JpegRenderer, self).__init__(**kwargs)
+
+    @property
+    def _options(self):
+        return ':{compression:d},{profile}'.format(
+            compression=self.compression,
+            profile=self.profile,
+        )
+
+    def render(self, image):
+        """Returns the rendered VIPS `image`."""
         if image.Bands() > 3:
             # Strip out alpha channel, otherwise transparent pixels turn white.
             image = image.extract_bands(band=0, nbands=3)
-        with NamedTemporaryFile(suffix='.jpg') as tempfile:
-            image.vips2jpeg(tempfile.name)
+        with NamedTemporaryFile(suffix=self.suffix) as tempfile:
+            image.vips2jpeg(tempfile.name + self._options)
             return tempfile.read()
 
 
 class PngRenderer(Renderer):
-    """Render a VIPS image as a PNG to filename."""
+    """
+    Render a VIPS image as a PNG.
+
+    compression: PNG compression level. Default 6.
+    interlace: Use ADAM7 interlacing. Default False.
+    suffix: Suffix for filename. Default '.png'.
+    """
     _suffix = '.png'
 
-    def render(self, image):
-        """
-        Renders the VIPS `image` to `filename`.
+    def __init__(self, compression=None, interlace=None, **kwargs):
+        if compression is None:
+            compression = 6
+        _compression = int(compression)
+        if not 0 <= _compression <= 9:
+            raise ValueError(
+                'compression must be between 0 and 9: {0}'.format(compression)
+            )
+        self.compression = _compression
 
-        Returns the filename actually rendered to.
-        """
-        with NamedTemporaryFile(suffix='.png') as tempfile:
-            image.vips2png(tempfile.name)
+        self.interlace = bool(interlace)
+
+        super(PngRenderer, self).__init__(**kwargs)
+
+    @property
+    def _options(self):
+        return ':{compression:d},{interlace:d}'.format(
+            compression=self.compression,
+            interlace=self.interlace,
+        )
+
+    def render(self, image):
+        """Returns the rendered VIPS `image`."""
+        with NamedTemporaryFile(suffix=self.suffix) as tempfile:
+            image.vips2png(tempfile.name + self._options)
             return tempfile.read()
 
 
