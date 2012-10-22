dd9fc0661fe2de44782bd87a533e3a6a2ba0761a
Simon Law <simon.law@ecometrica.com>
2012-10-22 16:29:13 -0400

    Remove testing code accidentally committed in the last change.

--- tests/test_renderers.py
+++ tests/test_renderers.py
@@ -47,40 +47,30 @@ class TestPngRenderer(unittest.TestCase):
     def test_simple(self):
         renderer = PngRenderer(png8=False, optimize=False)
         contents = renderer.render(image=self.image)
-        with open('/tmp/png/simple.png', 'wb') as f:
-            f.write(contents)
         self.assertEqual(intmd5(contents),
                          89446660811628514001822794642426893173)
 
     def test_compression(self):
         renderer = PngRenderer(compression=0, png8=False, optimize=False)
         contents = renderer.render(image=self.image)
-        with open('/tmp/png/compression.png', 'wb') as f:
-            f.write(contents)
         self.assertEqual(intmd5(contents),
                          12841159377787173134361510884891270318)
 
     def test_interlace(self):
         renderer = PngRenderer(interlace=1, png8=False, optimize=False)
         contents = renderer.render(image=self.image)
-        with open('/tmp/png/interlace.png', 'wb') as f:
-            f.write(contents)
         self.assertEqual(intmd5(contents),
                          197686704564132731296723533976357306757)
 
     def test_png8(self):
         renderer = PngRenderer(png8=True, optimize=False)
         contents = renderer.render(image=self.image)
-        with open('/tmp/png/png8.png', 'wb') as f:
-            f.write(contents)
         self.assertEqual(intmd5(contents),
                          151059771043192964835020617733646275057)
 
     def test_optimize(self):
         renderer = PngRenderer(png8=False, optimize=2)
         contents = renderer.render(image=self.image)
-        with open('/tmp/png/optimize.png', 'wb') as f:
-            f.write(contents)
         self.assertEqual(intmd5(contents),
                          86467695395038688928059075665951437140)
 
@@ -93,8 +83,6 @@ class TestPngRenderer(unittest.TestCase):
     def test_png8_optimize(self):
         renderer = PngRenderer(png8=True, optimize=2)
         contents = renderer.render(image=self.image)
-        with open('/tmp/png/png8_optimize.png', 'wb') as f:
-            f.write(contents)
         self.assertEqual(intmd5(contents),
                          151059771043192964835020617733646275057)
 
