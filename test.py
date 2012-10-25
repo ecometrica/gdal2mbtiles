aa687fb4ffd00455963ef39eec9296fa775dd275
Simon Law <simon.law@ecometrica.com>
2012-10-24 23:52:24 -0400

    Use Python 2.7's unittest discovery.

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
 import unittest
 
-from tests import *
-
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(module='tests')
--- tests/__init__.py
+++ tests/__init__.py
@@ -3,11 +3,10 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-from .test_gdal import *
-from .test_helpers import *
-from .test_mbtiles import *
-from .test_renderers import *
-from .test_scripts import *
-from .test_storages import *
-from .test_types import *
-from .test_vips import *
+import os
+
+
+def load_tests(loader, tests, pattern):
+    discovered = loader.discover(start_dir=os.path.dirname(__file__))
+    tests.addTests(tests=discovered)
+    return tests
