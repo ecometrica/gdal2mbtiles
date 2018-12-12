80d44f03766a43dfa42560cca6b31f062d82088e
faisal Albarazi <faisal.albarazi@ecometrica.com>
2018-12-12 10:49:40 -0500

    Moved constants from the spatial reference test to the constants file.

--- tests/test_spatial_reference.py
+++ tests/test_spatial_reference.py
@@ -3,28 +3,14 @@
 
 import rasterio
 import pytest
-from math import pi
 
 from numpy import array
 from numpy.testing import assert_array_almost_equal
 
-from gdal2mbtiles.constants import EPSG_WEB_MERCATOR
-
+from gdal2mbtiles.constants import (EPSG_WEB_MERCATOR,
+                                    EPSG3857_EXTENTS)
 from gdal2mbtiles.gdal import SpatialReference
 
-
-# SEMI_MAJOR is a constant referring to the WGS84 Semi Major Axis.
-SEMI_MAJOR = 6378137.0
-
-# Note: web-Mercator = pseudo-Mercator = EPSG 3857
-# The extents of the web-Mercator are constants.
-# Since the projection is formed from a sphere the extents of the projection
-# form a square.
-# For the values of the extents refer to:
-# OpenLayer lib: http://docs.openlayers.org/library/spherical_mercator.html
-EPSG3857_EXTENT = pi * SEMI_MAJOR
-
-EPSG3857_EXTENTS = array([[-EPSG3857_EXTENT]*2, [EPSG3857_EXTENT]*2])
 
 epsg_3857_raster_path = 'tests/web_mercator_3857.tif'
 
